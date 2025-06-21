d340387b899731770f0cfbabdd8604775dbe7a00
Manuel <manuramas1@gmail.com>
2025-06-21 20:40:44 +0200

    Schedule without logic

--- app/src/main/java/com/plcoding/vaccinationschedule/data/models/Diseases.kt
+++ app/src/main/java/com/plcoding/vaccinationschedule/data/models/Diseases.kt
@@ -1,15 +1,67 @@
 package com.plcoding.vaccinationschedule.data.models
 
-enum class Diseases(val displayName: String) {
-    POLIOMIELITIS("Poliomielitis"),
-    DIFTERIA_TETANOS_TOSFERINA("Difteria-Tetanos-Tosferina"),
-    HAEMOPHILUS_INFLUENZA_B("Haemophilus influenza B"),
-    HEPATITIS_B("Hepatitis B"),
-    SARAMPION_RUBEOLA_PAROTIDITIS("Sarampion-Rubeola-Parotiditis"),
-    ENFERMEDAD_MENINGOCOCICA("Enfermedad meningococica"),
-    VARICELA("Varicela"),
-    VIRUS_DEL_PAPILOMA_HUMANO("Virus del papiloma humano"),
-    ENFERMEDAD_NEUMOCOCICA("Enfermedad neumococica"),
-    HERPES_ZOSTER("Herpes zoster"),
-    GRIPE("Gripe")
+import androidx.compose.ui.graphics.Color
+import com.plcoding.vaccinationschedule.ui.theme.*
+import com.plcoding.vaccinationschedule.data.models.Ages.*
+
+enum class Diseases(
+    val displayName: String,
+    val backgroundColor: Color,
+    val vaccinationSchedule: List<Ages>
+) {
+    POLIOMIELITIS(
+        "Poliomielitis",
+        PoliomielitisColor,
+        listOf(TWO_MONTHS, FOUR_MONTHS, ELEVEN_MONTHS, SIX_YEARS)
+    ),
+    DIFTERIA_TETANOS_TOSFERINA(
+        "Difteria - Tetanos - Tosferina",
+        DifteriaColor,
+        listOf(PRENATAL, TWO_MONTHS, FOUR_MONTHS, ELEVEN_MONTHS, SIX_YEARS, FOURTEEN_YEARS, MORE_THAN_SIXTYFIVE_YEARS)
+    ),
+    HAEMOPHILUS_INFLUENZA_B(
+        "Haemophilus influenza B",
+        InfluenzaColor,
+        listOf(TWO_MONTHS, FOUR_MONTHS, ELEVEN_MONTHS)
+    ),
+    HEPATITIS_B(
+        "Hepatitis B",
+        HepatitisColor,
+        listOf(TWO_MONTHS, FOUR_MONTHS, ELEVEN_MONTHS)
+    ),
+    SARAMPION_RUBEOLA_PAROTIDITIS(
+        "Sarampion - Rubeola - Parotiditis",
+        SarampionColor,
+        listOf(TWELVE_MONTHS, FOUR_YEARS)
+    ),
+    ENFERMEDAD_MENINGOCOCICA(
+        "Enfermedad meningococica",
+        MeningococicaColor,
+        listOf(TWO_MONTHS, FOUR_MONTHS, TWELVE_MONTHS, TWELVE_YEARS)
+    ),
+    VARICELA(
+        "Varicela",
+        VaricelaColor,
+        listOf(FIFTEEN_MONTHS, FOUR_YEARS)
+    ),
+    VIRUS_DEL_PAPILOMA_HUMANO(
+        "Virus del papiloma humano",
+        PapilomaColor,
+        listOf(TWELVE_YEARS)
+    ),
+    ENFERMEDAD_NEUMOCOCICA(
+        "Enfermedad neumococica",
+        NeumocicaColor,
+        listOf(TWO_MONTHS, FOUR_MONTHS, SIX_MONTHS, ELEVEN_MONTHS, MORE_THAN_SIXTYFIVE_YEARS)
+    ),
+    HERPES_ZOSTER(
+        "Herpes zoster",
+        ZosterColor,
+        listOf(MORE_THAN_SIXTYFIVE_YEARS)
+    ),
+    GRIPE(
+        "Gripe",
+        GripeColor,
+        listOf(PRENATAL, SIX_MONTHS, ELEVEN_MONTHS, TWELVE_MONTHS, FIFTEEN_MONTHS, FOUR_YEARS, MORE_THAN_SIXTYFIVE_YEARS)
+    )
 }
--- app/src/main/java/com/plcoding/vaccinationschedule/vaccinationtable/VaccinationTableScreen.kt
+++ app/src/main/java/com/plcoding/vaccinationschedule/vaccinationtable/VaccinationTableScreen.kt
@@ -1,16 +1,20 @@
 package com.plcoding.vaccinationschedule.vaccinationtable
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +24,7 @@ import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.plcoding.vaccinationschedule.data.models.Ages
@@ -33,64 +38,97 @@ fun VaccinationTableScreen(
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        Box(
-            modifier = Modifier
-                .border(1.dp, Gray)
-                .width(110.dp)
-                .height(60.dp)
+        Column(
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text(
+            Row(
                 modifier = Modifier
-                    .padding(horizontal = 4.dp, vertical = 6.dp),
-                text = "VACUNACIÓN",
-                textAlign = TextAlign.Center
-            )
-        }
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .border(1.dp, Black)
-        ) {
-            for (age in Ages.entries) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                    .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                    .fillMaxWidth()
+            ) {
+                Box(
                     modifier = Modifier
-                        .height(60.dp)
                         .border(1.dp, Gray)
-                        .weight(1f)
+                        .width(130.dp)
+                        .height(60.dp)
                 ) {
                     Text(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 4.dp, vertical = 6.dp),
-                        text = age.displayName,
+                        modifier = Modifier.padding(horizontal = 12.dp, vertical = 20.dp),
+                        text = "VACUNACIÓN",
                         textAlign = TextAlign.Center
                     )
                 }
-            }
-        }
 
-        Column (
-            modifier = Modifier
-                .padding(16.dp)
-                .border(1.dp, Black)
-        ) {
-            for (disease in Diseases.entries) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(110.dp)
-                        .border(1.dp, Gray)
-                        .weight(1f)
-                ) {
-                    Text(
+                Column(modifier = Modifier.height(60.dp)) {
+                    Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(horizontal = 4.dp, vertical = 6.dp),
-                        text = disease.displayName,
-                        textAlign = TextAlign.Center
-                    )
+                            .height(30.dp)
+                            .weight(1f)
+                            .border(1.dp, Black)
+                    ) {
+                        for (age in Ages.entries) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier
+                                    .height(60.dp)
+                                    .border(1.dp, Gray)
+                                    .weight(1f)
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 4.dp, vertical = 6.dp),
+                                    text = age.displayName,
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 14.sp
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .padding(start = 10.dp, bottom = 8.dp)
+                    .border(1.dp, Black)
+            ) {
+                for (disease in Diseases.entries) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(end = 10.dp)
+                            .border(1.dp, Gray)
+                            .weight(1f)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .background(disease.backgroundColor)
+                        ) {
+                            Text(
+                                modifier = Modifier
+                                    .width(130.dp)
+                                    .padding(horizontal = 4.dp, vertical = 12.dp),
+                                text = disease.displayName,
+                                textAlign = TextAlign.Center,
+                                fontSize = 14.sp
+                            )
+                        }
+                        for (age in Ages.entries) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .border(1.dp, Gray)
+                                    .weight(1f)
+                                    .clickable {
+                                        println("Clicked on $age for $disease")
+                                    }
+                            )
+                        }
+                    }
                 }
             }
         }
